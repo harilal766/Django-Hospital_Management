d900e315328d8fc0047e6e5e89a42587156d426c
Hari <harilalsunil2@gmail.com+harilal766@users.noreply.github.com>
2024-07-27 02:20:59 +0530

    updated dashboard.html

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from consultation.models import Appointment,Doctor,Nurse,Pharmacist
+from consultation.models import Appointment,Doctor,Nurse,Pharmacist,Patient
 # Create your views here.
+site_name = 'Health Center'
 def dashboard(request):
     # Number of healthcare professionals present on a working day
     doctors = Doctor.objects.all().count
     nurses = Nurse.objects.all().count
     pharmacists = Pharmacist.objects.all().count
+    patients = Patient.objects.all().count
 
     appoint = Appointment.objects.all()
 
@@ -13,6 +15,8 @@ def dashboard(request):
         'doctors':doctors,
         'nurses':nurses,
         'pharmacists':pharmacists,
-        'appoint':appoint
+        'patients':patients,
+        'appoint':appoint,
+        'name' :site_name
         }
     return render(request,"dashboard.html",context)
