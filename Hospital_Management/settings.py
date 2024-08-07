23f0f748b1ebc0f2b288e28f37387d2c7969781c
harilal766 <harilalsunil2@gmail.com+harilal766@users.noreply.github.com>
2024-08-07 23:33:54 +0530

    working on profile

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #Apps
-    'consultation','dashboard',
+    'consultation','dashboard','userauth',
     # allauth 
     'allauth','allauth.account',
     'allauth.socialaccount.providers.google',
--- Hospital_Management/urls.py
+++ Hospital_Management/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('consultation.urls')),
+    path('user/',include('userauth.urls')),
     path('accounts/', include('allauth.urls')),
     path('dashboard',include('dashboard.urls')),
 ]
