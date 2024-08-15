f65d2ee751163b7e82243673d376d86bba2941d0
harilal766 <harilalsunil2@gmail.com>
2024-08-16 01:07:26 +0530

    railway

--- Hospital_Management/settings.py
+++ Hospital_Management/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qoq(+!lo!k_nes-e^d-y2)18+ogajkv@zp+75&v_yjyf895x4l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ["*"]
 
 # Media directory
 MEDIA = '/media/'
