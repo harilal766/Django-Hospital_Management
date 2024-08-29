098d717de55b514de1b1f6cf21d67166ce38fc39
harilal766 <harilalsunil2@gmail.com>
2024-08-29 20:57:51 +0530

    added a devloper option in nav bar

--- consultation/models.py
+++ consultation/models.py
@@ -18,7 +18,7 @@ class Status(models.Model):
 
 class Department(models.Model):
     name = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
     def __str__(self):
         return self.name
     
